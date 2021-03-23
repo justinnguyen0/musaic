66c214d673c7d13a6d00d782950d88cd3bf192c0
Jeffrey Sung <jjsung@berkeley.edu>
2021-03-23 01:00:55 -0700

    organize

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
-import logo from './logo.svg';
+import logo from './images/logo.svg';
 import './App.css';
 import Login from './components/Login/Login';
 import NavBar from './components/NavBar/NavBar';
