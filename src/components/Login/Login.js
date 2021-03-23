4c122f8d2bd71821450d56128d5883a1e45eaa76
boone <54647789+boone-p@users.noreply.github.com>
2021-03-23 16:38:52 -0700

    Update Login.js

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,67 +1,133 @@
-import React, {Component} from "react"
+import React, { useState } from 'react';
+import styled, { createGlobalStyle, css } from 'styled-components';
 
-class Login extends Component {
-    constructor(props) {
-        super(props)
+const GlobalStyle = createGlobalStyle`
+  html {
+    height: 100%
+  }
+  body {
+    font-family: Encode Sans Expanded;
+    background: #fcf2d1;;
+    height: 100%;
+    margin: 0;
+    color: #555;
+  }
+`;
 
-        this.state = {
-            username:"",
-            password:"",
-            confirmPassword:"",
-        }
+const sharedStyles = css`
+    background-color: #eee;
+    height: 40px;
+    border-radius: 5px;
+    border: 1px solid #ddd;
+    margin: 10px 0 20px 0;
+    padding: 20px;
+    box-sizing: border-box;
+`;
 
-        this.handleSubmit = this.handleSubmit.bind(true)
-    }
+const StyledFormWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    padding: 0 20px;
+`;
 
-    usernameHandler = (event) => {
-        this.setState({
-            username: event.target.value
-        })
-    }
+const StyledForm = styled.form`
+    width: 100%;
+    max-width: 700px;
+    padding: 40px;
+    background-color: #fff;
+    border-radius: 10px;
+    box-sizing: border-box;
+    box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.2);
+`;
 
-    passwordHandler = (event) => {
-        this.setState({
-            password: event.target.value
-        })
-    }
+const StyledInput = styled.input`
+    font-size: 0.9rem;
+    display: block;
+    width: 100%;
+    ${sharedStyles}
+`;
 
-    confirmPasswordHandler = (event) => {
-        this.setState({
-            confirmPassword: event.target.value
-        })
-    }
+const StyledButton = styled.button`
+    display: block;
+    background-color: #f7797d;
+    color: #fff;
+    font-size: 0.9rem;
+    border: 0;
+    border-radius: 10px;
+    height: 40px;
+    padding: 0 20px;
+    cursor: pointer;
+    box-sizing: border-box;
+`;
 
-    handleSubmit = (event) => {
-        alert(`${this.state.username} Login Successful`)
-        console.log(this.state)
-        this.setState({
-            username:"",
-            password:"",
-            confirmPassword:"",
+const StyledError = styled.div`
+    color: red;
+    font-weight: 800;
+    margin: 0 0 40px 0;
+`;
 
-        })
-    event.preventDefault()
-    }
+const initalState = {
+    email: '',
+    password: '',
+};
 
+function Login() {
+    const [state, setState] = useState(initalState);
+    const [error, setError] = useState('');
 
+    const handleSubmit = e => {
+        e.preventDefault();
+        //validate email and password
+        //redirect to homepages
+        setError('');
+        alert('Testing: see comments in handlesubmit')
+        console.log("Success")
+    };
 
-render() {
-    return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    <h1>User Login</h1>
-                    <label>Username :</label> <input type="text" value={this.state.username} onChange={this.usernameHandler} placeholder="Username..." /><br />
-                    <label>Password :</label> <input type="password" value={this.state.password} onChange={this.passwordHandler} placeholder="Password..." /><br />
-                    <input type="submit" value="Submit" />
-                </form>
-                <div className="App">
-                    <a href="http://localhost:8888">
-                        <button>Login with Spotify</button>
-                    </a>
-                </div>
-            </div>
-        )
-    }
+    const handleInput = e => {
+        const inputName = e.currentTarget.name;
+        const value = e.currentTarget.value;
+
+        setState(prev => ({ ...prev, [inputName]: value }));
+    };
+
+  return (
+    <>
+        <GlobalStyle />
+        <StyledFormWrapper>
+            <StyledForm onSubmit={handleSubmit}>
+                <h2>Login</h2>
+
+                <label htmlFor="email">Email</label>
+                <StyledInput
+                    type="email"
+                    name="email"
+                    value={state.email}
+                    onChange={handleInput}
+                />
+
+                <label htmlFor="password">Password</label>
+                <StyledInput
+                    type="password"
+                    name="password"
+                    value={state.password}
+                    onChange={handleInput}
+                />
+                <h5><a href="http://localhost:3000/register">Forgot your password?</a></h5>
+                
+                {error && (
+                    <StyledError>
+                    <p>{error}</p>
+                    </StyledError>
+                )}
+                
+                <StyledButton onClick={handleSubmit} type="submit">Login</StyledButton>
+                </StyledForm>
+        </StyledFormWrapper>
+    </>
+  );
 }
 
-export default Login;
+export default Login
